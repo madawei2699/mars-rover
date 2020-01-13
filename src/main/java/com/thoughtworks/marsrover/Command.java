e550d5883e31ea5f8474aa5fe357df61d1176acb
madawei2699 <madawei2699@gmail.com>
2020-01-14 00:01:08 +0800

    add Location test with impl

--- src/main/java/com/thoughtworks/marsrover/Command.java
+++ src/main/java/com/thoughtworks/marsrover/Command.java
@@ -9,6 +9,56 @@ public enum Command {
     }
 
     public Location execute(Location current) {
-        return new Location(0, 0, Direction.N);
+        switch (this) {
+            case L:
+                switch (current.direction) {
+                    case N:
+                        current.direction = Direction.W;
+                        break;
+                    case E:
+                        current.direction = Direction.N;
+                        break;
+                    case S:
+                        current.direction = Direction.E;
+                        break;
+                    case W:
+                        current.direction = Direction.S;
+                        break;
+                }
+                break;
+            case R:
+                switch (current.direction) {
+                    case N:
+                        current.direction = Direction.E;
+                        break;
+                    case E:
+                        current.direction = Direction.S;
+                        break;
+                    case S:
+                        current.direction = Direction.W;
+                        break;
+                    case W:
+                        current.direction = Direction.N;
+                        break;
+                }
+                break;
+            case M:
+                switch (current.direction) {
+                    case N:
+                        current.y += 1;
+                        break;
+                    case E:
+                        current.x += 1;
+                        break;
+                    case S:
+                        current.y -= 1;
+                        break;
+                    case W:
+                        current.x -= 1;
+                        break;
+                }
+                break;
+        }
+        return current;
     }
 }
--- src/test/java/com/thoughtworks/marsrover/LocationTest.java
+++ src/test/java/com/thoughtworks/marsrover/LocationTest.java
@@ -83,4 +83,44 @@ public class LocationTest {
         // then
         assertEquals(Direction.W, newLocation.direction);
     }
+
+    @Test
+    public void should_increse_x_when_move_from_E() {
+        // given
+        Location currentLocation = new Location(0, 0, Direction.E);
+        // when
+        Location newLocation = Command.M.execute(currentLocation);
+        // then
+        assertEquals(1, newLocation.x);
+    }
+
+    @Test
+    public void should_decrease_x_when_move_from_W() {
+        // given
+        Location currentLocation = new Location(0, 0, Direction.W);
+        // when
+        Location newLocation = Command.M.execute(currentLocation);
+        // then
+        assertEquals(-1, newLocation.x);
+    }
+
+    @Test
+    public void should_increse_y_when_move_from_N() {
+        // given
+        Location currentLocation = new Location(0, 0, Direction.N);
+        // when
+        Location newLocation = Command.M.execute(currentLocation);
+        // then
+        assertEquals(1, newLocation.y);
+    }
+
+    @Test
+    public void should_decrease_y_when_move_from_S() {
+        // given
+        Location currentLocation = new Location(0, 0, Direction.S);
+        // when
+        Location newLocation = Command.M.execute(currentLocation);
+        // then
+        assertEquals(-1, newLocation.y);
+    }
 }
